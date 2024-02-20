8f93de9f0b5bccdbe36278284f83f7fc020dcb78
fbrueckl <60046427+fbrueckl@users.noreply.github.com>
2024-02-20 18:12:29 +0100

    Added basic html

--- attackPlanManager.js
+++ attackPlanManager.js
@@ -37,12 +37,16 @@ var scriptConfig = {
             Help: 'Help',
             'Attack Plan Manager': 'Attack Plan Manager',
             'There was an error!': 'There was an error!',
+            'Import attack plan:': 'Import attack plan:',
+            'Import': 'Import',
         },
         de_DE: {
             'Redirecting...': 'Weiterleiten...',
             Help: 'Hilfe',
-            'Attack Plan Manager': 'Attack Plan Manager',
+            'Attack Plan Manager': 'Angriffsplan Manager',
             'There was an error!': 'Es gab einen Fehler!',
+            'Import attack plan:': 'Angriffsplan importieren:',
+            'Import': 'Importieren',
         }
     }
     ,
@@ -93,6 +97,137 @@ $.getScript(`https://twscripts.dev/scripts/twSDK.js?url=${document.currentScript
                 console.error(`${scriptInfo}: Error:`, error);
             }
         })();
+
+        function renderUI() {
+            const style = generateCSS();
+            const importContent = generateImport();
+
+            let content = `
+                <div>
+                    ${importContent}
+                </div>
+            `
+            twSDK.renderBoxWidget(
+                content,
+                'AttackPlanManager',
+                'attack-plan-manager',
+                style
+            );
+
+        }
+
+        function addEventHandlers() {
+
+        }
+
+        function generateCSS() {
+
+            let css = `
+                    .sb-grid-5 {
+                        grid-template-columns: repeat(5, 1fr);
+                    }
+                    .sb-grid-4 {
+                        grid-template-columns: repeat(4, 1fr);
+                    }
+                    .sb-grid-3 {
+                        grid-template-columns: repeat(3, 1fr);
+                    }
+                    .sb-grid-2 {
+                        grid-template-columns: repeat(2, 1fr);
+                    }
+                    .sb-grid-20-80 {
+                        grid-template-columns: 20% 80%;
+                    }
+                    .sb-grid-25-25-50 {
+                        grid-template-columns: calc(25% - 5px) calc(25% - 5px) calc(50% - 10px);
+                    }
+                    .sb-grid {
+                        display: grid;
+                        grid-gap: 10px;
+                    }
+                    .sb-grid {
+                        display: grid;
+                        grid-gap: 10px;
+                    }
+                    fieldset {
+                        border: 1px solid #c1a264;
+                        border-radius: 4px;
+                        padding: 9px;
+                    }
+                    legend {
+                        font-size: 12px; 
+                        font-weight: bold; 
+                    }
+                    input[type="number"] {
+                        padding: 8px;
+                        font-size: 14px;
+                        border: 1px solid #c1a264;
+                        border-radius: 3px;
+                        width: 90px;
+                    }
+                    input[type="checkbox"] {
+                        margin-right: 5px;
+                        transform: scale(1.2);
+                    }
+                    input[type="email"] {
+                        padding: 8px;
+                        font-size: 11px;
+                        border: 1px solid #c1a264;
+                        border-radius: 3px;
+                        width: 100%; 
+                    }
+                    input[type="email"]::placeholder { 
+                        font-style: italic;
+                        font-size: 10px;
+                    }
+                    input[type="number"]::-webkit-inner-spin-button,
+                    input[type="number"]::-webkit-outer-spin-button,
+                    input[type="number"] {
+                        -webkit-appearance: none;
+                        margin: 0;
+                    }
+                    input[type="number"] {
+                        -moz-appearance: textfield;
+                    }
+                    input[type="number"]:focus,
+                    input[type="checkbox"]:focus,
+                    input[type="email"]:focus {
+                        outline: none;
+                        border-color: #92794e;
+                        box-shadow: 0 0 5px rgba(193, 162, 100, 0.7);
+                    }
+                    select {
+                        padding: 8px;
+                        font-size: 14px;
+                        border: 1px solid #c1a264;
+                        border-radius: 3px;
+                        width: 165px;
+                    }
+                    select:hover {
+                        border-color: #92794e; 
+                    }
+                    
+                    select:focus {
+                        outline: none;
+                        border-color: #92794e; 
+                        box-shadow: 0 0 5px rgba(193, 162, 100, 0.7);
+                    }
+
+            `;
+
+            return css;
+        }
+
+        function generateImport() {
+            const fieldset = `
+                <fieldset>
+                    <legend>${twSDK.tt('Import attack plan:')}</legend>
+                    <textarea id="importInput" class="sb-input-textarea"></textarea>
+                    <button id="importPlan" class="btn">${twSDK.tt('Import')}</button>
+                </fieldset>
+            `;
+            return fieldset;
+        }
 
     }
 );
